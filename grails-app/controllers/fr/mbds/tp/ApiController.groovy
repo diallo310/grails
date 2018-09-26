aea3bbf0a5376fe672da733ed66ec4405c04cb06
diallo310 <diallohawaou310@gmail.com>
2018-09-26 08:17:45 +0200

    ajout de L API

--- grails-app/controllers/fr/mbds/tp/ApiController.groovy
+++ grails-app/controllers/fr/mbds/tp/ApiController.groovy
@@ -4,35 +4,63 @@ import grails.converters.JSON
 import grails.converters.XML
 
 class ApiController {
+    UserService userService
 
-    def index() {
-      //  render"ok"  
-       /* switch (request.getMethod()){
-            case"POST":
-                render "post"
-                println request.getHeader('Allow :text/xml')
-                break;
-            case"GET":
-                render"get"
-                break;
+    def user() {
+        switch (request.getMethod()) {
+            case "POST":
+                if (new User(request.JSON).save(flush: true)) {
+                    response.status = 200
+                } else {
+                    response.status = 404
+                }
+                break
+            case "GET":
+                if (params.id) {
+                    if (userService.get(params.id)) {
+                        render userService.get(params.id) as JSON
+                    } else {
+                        render(status: 404, 'Not Found')
+                    }
 
-        }*/
-    }
+                } else {
+                    if (userService.list(params)) {
+                        render userService.list(params) as JSON
+                    } else {
+                        render(status: 404, 'Not Found')
+                    }
+                }
+                break
+            case "PUT":
+                def user = userService.get(request.JSON.id)
+                if (user) {
+                    user.properties=request.JSON
+                    if (user.save(flush: true)) {
+                        response.status = 200
+                    }
 
-    def user(){
-        switch (request.getMethod()){
-            case"POST":
-                  def userInstance = new User(params)
+                } else {
+                    response.status = 404
+                }
 
-                if(userInstance.save(flush:true))
-                    response.status=201
-                else
-                    response.status=400
-                break;
-            default:
-                response.status=405
-                break;
+                break
+            case "DELETE":
+                def user = userService.get(request.JSON.id)
+                if (user) {
+                    userService.delete(request.JSON.id)
+                    if(request.JSON.id){
+                        response.status=200
+                    }else{
+
+                    }
+                }else{
+                    response.status=404
+                }
+                break
+
         }
+
     }
+
 }
 
